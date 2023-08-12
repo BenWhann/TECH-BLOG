5ba8df29d214698371c9e5dc8a3436d4d330a9bc
BenWhann <whann004@umn.edu>
2023-08-12 14:49:08 -0500

    set up server file

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+
+// gets all users
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// creates a new user
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
