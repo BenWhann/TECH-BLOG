669b24c365af9e0dc1d52ce4f7426ac6b1d91529
BenWhann <whann004@umn.edu>
2023-08-13 19:26:11 -0500

    quic save

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -29,11 +29,10 @@ router.post('/', withAuth, async (req, res) => {
         return;
       }
   
-      res.status(200).json(postData);
+      res.status(200).json(commentData);
     } catch (err) {
       res.status(500).json(err);
     }
   });
 
   module.exports = router;
-  
